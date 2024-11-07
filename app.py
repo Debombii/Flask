50d09cfb25e29bd76c314acc5bcfc96395d728e5
Debombii <158999291+Debombii@users.noreply.github.com>
2024-11-07 10:09:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
-
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
